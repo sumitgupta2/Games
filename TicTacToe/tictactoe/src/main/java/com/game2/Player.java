8135e545ee8840552fc89525aca26fe4ede5f9bd
Sumit <sumitgupta_1987@yahoo.co.in>
2024-01-27 13:33:29 +0530

    Added SnakeNLadder classes

---
+++ TicTacToe/tictactoe/src/main/java/com/game2/Player.java
@@ -0,0 +1,17 @@
+package com.game2;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Player {
+    private String playerName;
+    private int playerId;
+    private  String address;
+    private  int ranking;
+    private  char playerSymbol;
+
+}
